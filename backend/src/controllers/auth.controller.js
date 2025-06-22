ca1c7e2e392ce820fc35654e07abbacd20118c5f
Ashutosh Gupta <ashutoshgupta2737@gmail.com>
2025-06-23 00:57:11 +0530

    added login logout controllers and added Stream config

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
+import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
+import { upsertStreamUser } from "../libs/stream.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -42,6 +44,17 @@ export const signup = async (req, res) => {
     const userToSend = newUser.toObject();
     delete userToSend.password;
 
+    try {
+      await upsertStreamUser({
+        id: newUser._id.toString(),
+        name: newUser.fullName,
+        image: newUser.profilePic || ""
+      })
+      console.log(`Stream user created for ${newUser.fullName}`)
+    } catch (error) {
+      console.error("Error creating Stream user", error)
+    }
+
     const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
@@ -63,6 +76,59 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-export const logout = async (req, res) => {};
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const isPasswordValid = await user.matchPassword(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production"
+    })
+
+    res.status(200).json({
+        message: "Login successful",
+        user
+    })
+  } catch (error) {
+    console.error("Error in login", error)
+    res.status(500).json({message: "Internal server error"})
+  }
+};
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("jwt")
+        res.status(200).json({message: "Logout successfui"})
+    } catch (error) {
+        console.error("Error in logout", error)
+        res.status(500).json({message: "Internal server error"})
+    }
+};
+
